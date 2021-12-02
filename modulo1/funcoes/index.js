adf30345140f91a1c3923b15708a8c94b4e04740
thiagohenrique23 <thiagohenrique2309@gmail.com>
2021-12-02 16:26:52 -0300

    funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,64 @@
+console.log("Olá mundo");
+
+// Exercícios de interpretação 1
+ 
+// A) Vai ser impresso 10 e 50. 
+
+// B) Não irá aparecer nada.
+
+// Exercícios de interpretação 2
+
+// A) A função irá deixar tudo em minúsculo e verificar se está escrito cenoura se não = false, se sim = true
+
+// B) 1 = true 2 = false 3 = false
+
+// Exercícios de escrita 1
+
+// console.log("Eu sou Thiago, tenho 19 anos, moro em Goiânia e sou estudante.");
+
+// function minhafuncao(nome,idade,cidade,profissao) {
+// 
+//   console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`)
+//   
+//   
+// }
+// minhafuncao("Thiago",19,"Goiânia","estudante");
+
+// function somar(primeiroNumero,segundoNumero) {
+// 
+//   const soma = primeiroNumero + segundoNumero
+//   console.log(`A soma dos números é ${primeiroNumero}`);
+// 
+//   return soma
+// }
+// somar(1 + 2)
+ 
+// function maior(primeiroNumero,segundoNumero) {
+// 
+//   const maior = primeiroNumero + segundoNumero
+//   console.log(`O número é maior ou igual? ${primeiroNumero}`);
+// 
+//   return maior
+// }
+// maior(1 >= 2)
+
+// function par(primeiroNumero,segundoNumero) {
+// 
+//      const par = primeiroNumero + segundoNumero
+//      console.log(`O número é par? ${primeiroNumero}`);
+// 
+//    return par
+//  }
+//  par(2 === 2)
+
+// function numeros(numeroum,numerodois) {
+// 
+//   const numeros = numeroum + numerodois
+//   console.log("A soma dos dois é",numeroum + numerodois);
+//   console.log("Tem difereça dos dois?",numeroum != numerodois);
+//   console.log("A multiplicação dos dois é",numeroum * numerodois);
+//   console.log("A divisão dos dois é",numeroum / numerodois);
+// 
+//  return numeros
+// }
+// numeros(5,6)
