e0d30d09a9e2ca6fedde8588e8797182989713ce
thiagohenrique23 <thiagohenrique2309@gmail.com>
2021-12-03 18:42:15 -0300

    Projeto

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,137 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+// function soma(num1, num2) {
+  
+//  return num1 + num2
+// }
+
+// EXERCÍCIO 0B
+// function imprimeMensagem() {
+  
+//  const mensagem = prompt('Digite uma mensagem!')
+
+//  console.log(mensagem)
+//}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+// function calculaAreaRetangulo(){
+//   
+//  const altura = Number(prompt(" altura"));
+//  const largura = Number(prompt(" largura"));
+// 
+//  console.log( altura * largura)
+// 
+// }
+
+
+
+// EXERCÍCIO 02
+//  function imprimeIdade() {
+//   
+//    const anoAtual = Number(prompt(" ano atual"))
+//    const anoNascimento = Number(prompt(" ano de nascimento"))
+//    
+//    console.log(anoAtual - anoNascimento)
+//
+// }
+ 
+// // EXERCÍCIO 03
+// function calculaIMC(peso, altura) {
+//   
+//  const resultado = (peso / (altura * altura)).toFixed(1)
+//  
+//  return resultado
+// }
+//
+ 
+
+// 
+// // EXERCÍCIO 04
+// function imprimeInformacoesUsuario() {
+//
+//  const nome = prompt("Digite seu nome")
+//  const idade = prompt("Digite sua idade")
+//  const email = prompt("Digite seu email")
+//
+//  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 05
+// function imprimeTresCoresFavoritas() {
+//  
+//  const corum = prompt("Digite uma cor favorita")
+//  const cordois = prompt("Digite uma cor favorita diferente")
+//  const cortres = prompt("Digite uma cor favorita diferente")
+//  
+//  const cores = [corum, cordois, cortres]
+//  console.log(cores)
+//  
+//  } 
+
+// // EXERCÍCIO 06
+ function retornaEmMaiuscula(palavra) {
+   
+  const mensagem = "Markting Digital".toLocaleUpperCase()
+  return mensagem
+ }
+ 
+
+// 
+// // EXERCÍCIO 07
+// function calculaIngressosEspetaculo(custo, valorIngresso) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 08
+// function checaStringsMesmoTamanho(string1, string2) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 09
+// function retornaPrimeiroElemento(array) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 10
+// function retornaUltimoElemento(array) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 11
+// function trocaPrimeiroEUltimo(array) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 12
+// function checaIgualdadeDesconsiderandoCase(string1, string2) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 13
+// function checaRenovacaoRG() {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 14
+// function checaAnoBissexto(ano) {
+//   
+// 
+// }
+// 
+// // EXERCÍCIO 15
+// function checaValidadeInscricaoLabenu() {
+//   
+// 
+// }
