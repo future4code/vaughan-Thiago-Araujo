13f69b1038223aff930911dbd64eee284ec5e1cd
thiagohenrique23 <thiagohenrique2309@gmail.com>
2021-12-06 17:42:38 -0300

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,85 @@
+console.log("Olá mundo")
+
+// Exercícios de interpretação 1
+
+// Vai ser impresso: Matheus Nachtergaele, Virginia Cavendish e Globo 14h 
+
+// Exercícios de interpretação 2
+
+// A) Vai ser impresso:
+// nome: Juca, idade: 3, raça: SRD
+// nome: Juba, idade: 3, raça: SRD
+// nome: Jubo, idade: 3, raça: SRD
+
+// B) Serve basicamente para você dizer que podem ser passados diversos parametros naquela função.
+
+// Exercícios de interpretação 3
+
+// A) Vai ser impresso: false e undefined
+
+// B) Porque não foi atribuido nenhum valor a altura 
+
+// Exercícios de escrita 1
+
+// const pessoa = {
+// 
+//   nome : "Thiago",
+//   apelidos: ["Thi ", "Tato ", "Th "],
+// 
+// }
+//
+// const novosApelidos = {
+//
+//  novosApelidos: [" Senhor"," Lorde", " M45TER"]
+// }
+// 
+// console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${novosApelidos.novosApelidos}`)
+
+// Exercícios de escrita 2
+
+// const nomeUm = {
+// 
+//   nome: "Leando",
+//   idade: 25,
+//   profissao: "Cantor"
+// 
+// }
+// 
+// const nomeDois = {
+// 
+//   nome: "José",
+//   idade: 24,
+//   profissao: "Cantor"
+// 
+// }
+// 
+// console.log(`${nomeUm.nome}, ${nomeUm.nome.length}, ${nomeUm.idade}, ${nomeUm.profissao}, ${nomeUm.profissao.length}`);
+// console.log(`${nomeDois.nome}, ${nomeDois.nome.length}, ${nomeDois.idade}, ${nomeDois.profissao}, ${nomeDois.profissao.length}`);
+
+// Exercícios de escrita 3
+
+// let carrinho = []
+// 
+// const frutaUm = {
+//   nome: "Laranja",
+//   frutaUm: true
+//   
+// }
+// 
+// const frutaDois = {
+//   nome: "Melacia",
+//   frutaDois: true
+// }
+// 
+// const frutaTres = {
+//   nome: "Maçã",
+//   frutaTres: true
+// }
+// 
+// function recebaObjeto(frutaUm,frutaDois,frutaTres){
+//  carrinho.push(frutaUm, frutaDois, frutaTres);
+//   
+// }
+// 
+// recebaObjeto(frutaUm,frutaDois,frutaTres);
+// console.log(carrinho);
