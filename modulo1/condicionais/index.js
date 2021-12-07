0af3fa282a70d11530db5788abf0f120878f2159
thiagohenrique23 <thiagohenrique2309@gmail.com>
2021-12-07 17:01:43 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,99 @@
+// Exercícios de interpretação 1
+
+// A) O código pede ao usúario para que ele digite um número para testar, em seguida ele retorna o resto da divisão por 2,
+// se o valor for igual a 0 passou no teste se der valor diferente não passou no teste.
+
+// B) Todo número que for igual a 0
+
+// C) Todo número que for diferente de 0
+
+// Exercícios de interpretação 2
+
+// A) O código serve para digitar uma fruta e dizer o valor dela 
+
+// B) O preço da fruta Maçã é R$ 2.25
+
+// C) O preço da fruta Pêra é R$ 5
+
+// Exercícios de interpretação 3
+
+// A) pede a usúario para que ele digite um número
+
+// B) Esse número passou no teste, iria dar erro por falta do else
+
+// C) Sim, o let mensagem está dentro de um if então ele não retornará a mensagem secreta.
+
+// Exercícios de escrita 1
+
+// const idade = Number(prompt("Digite sua idade"))
+// 
+// if (idade >= 18) {
+// 
+//   console.log("Você pode dirigir");
+// 
+// } else {
+// 
+//   console.log("Você não pode dirigir")
+// 
+// }
+
+// Exercícios de escrita 2
+
+// const periodo = prompt("Qual o seu período? M (matutino) ou V (Vespertino) ou N (Noturno)")
+// 
+// if (periodo === "M") {
+// 
+//   console.log("Bom dia!")
+// 
+// } else if (periodo === "V") {
+// 
+//   console.log("Boa Tarde!")
+// 
+// } else if (periodo === "N") {
+// 
+//   console.log("Boa Noite!")
+// 
+// }
+
+// Exercícios de escrita 3
+
+// const periodo = prompt("Qual o seu período? M (matutino) ou V (Vespertino) ou N (Noturno)")
+// 
+// switch (periodo) {
+//   case "M":
+//       console.log("Bom dia!")
+//       break
+//    
+//   case "V":
+//       console.log("Boa tarde!")
+//       break
+// 
+//   case "N":
+//       console.log("Boa noite!")
+//       break      
+// }
+
+// Exercícios de escrita 4
+
+// const filme = prompt("Qual o gênero de filme que vão assistir?");
+// const preco = prompt("Qual o preço que gostaria de pagar?");
+// const lanche = prompt("Gostaria de uma pipoca?")
+// 
+// if (filme === "fantasia", preco <= 15,lanche === "não") {
+// 
+//   console.log("Bom filme!")
+// 
+// } else if (filme === "fantasia", preco <= 15, lanche === "sim") {
+// 
+//   console.log("Bom filme!")
+//   console.log("Aproveite a pipoca!")
+// 
+// } else {
+// 
+//   console.log("Escolha outro filme :(")
+// 
+// }
+
+// Desafio 1 ↑
+
+// Desafio 2
