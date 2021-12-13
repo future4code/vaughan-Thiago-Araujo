639a6f80b711a387f91df48142880759a67464ed
thiagohenrique23 <thiagohenrique2309@gmail.com>
2021-12-13 13:42:20 -0300

    execicio1

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,98 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+  return array.length
+}
+retornaTamanhoArray("Azul","Amarelo","Vermelho")
+console.log(array)
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  return array.reverse()
+}
+
+console.log(retornaArrayInvertido([0,8,23,16,10,15,41,12,13]))
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  return array.sort((a, b) => a - b)
+}
+
+console.log(retornaArrayOrdenado([36,12,56,7,3]))
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  
+
+}
+
+console.log(retornaNumerosPares([1,2,3,4,5,6,7,8]))
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
