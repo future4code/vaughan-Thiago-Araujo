c48508464bba5dd7a084fe2b665f7646345effc4
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-02-01 10:44:05 -0300

    XD

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components'
 
 const AppContainer = styled.div`
   border: 1px solid black;
@@ -9,75 +9,73 @@ const AppContainer = styled.div`
   margin: auto;
   display: flex;
   flex-direction: column;
-`;
+`
 
 const MessagesContainer = styled.div`
   flex-grow: 1;
   padding: 16px;
   display: flex;
   flex-direction: column-reverse;
-`;
+`
 
 const InputsContainer = styled.div`
   display: flex;
-`;
+`
 
 const NameInput = styled.input`
   width: 100px;
-`;
+`
 
 const MessageInput = styled.input`
   flex-grow: 1;
-`;
+`
 
 class App extends React.Component {
   state = {
     messages: [],
-    userValue: "",
-    messageValue: ""
-  };
+    userValue: '',
+    messageValue: ''
+  }
 
   onChangeUserValue = (event) => {
-    this.setState({ userValue: event.target.value });
-  };
+    this.setState({userValue: event.target.value})
+  }
 
   onChangeMessageValue = (event) => {
-    this.setState({ messageValue: event.target.value });
-  };
+    this.setState({messageValue: event.target.value})
+  }
 
   sendMessage = () => {
     const newMessage = {
       user: this.state.userValue,
       text: this.state.messageValue
-    };
+    }
 
-    const newMessagesArray = [newMessage, ...this.state.messages];
+    const newMessagesArray = [newMessage, ...this.state.messages]
 
-    this.setState({ messages: newMessagesArray, messageValue: "" });
-  };
+    this.setState({messages: newMessagesArray, messageValue: ''})
+  }
 
   render() {
     return (
       <AppContainer>
         <MessagesContainer>
           {this.state.messages.map((message, index) => {
-            return (
-              <p key={index}>
-                <strong>{message.user}</strong>: {message.text}
-              </p>
-            );
+            return <p key={index}>
+              <strong>{message.user}</strong>: {message.text}
+            </p>
           })}
         </MessagesContainer>
         <InputsContainer>
           <NameInput
-            onChange={this.onChangeUserValue}
-            value={this.state.userValue}
-            placeholder={"Nome"}
+            onChange={this.onChangeUserValue} 
+            value={this.state.userValue} 
+            placeholder={'Nome'}
           />
           <MessageInput
-            onChange={this.onChangeMessageValue}
-            value={this.state.messageValue}
-            placeholder={"Mensagem"}
+            onChange={this.onChangeMessageValue} 
+            value={this.state.messageValue} 
+            placeholder={'Mensagem'}
           />
           <button onClick={this.sendMessage}>Enviar</button>
         </InputsContainer>
