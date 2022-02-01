7c02a079ccca15985deb9fda8409552a9ef5318d
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-02-01 11:21:35 -0300

    demorou

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -36,11 +36,14 @@ console.log(par);
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-  let parElevado = array.filter(parElevado => (parElevado % 2 )== 0 )
-  return parElevado
+  let novoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      novoArray.push(array[i] * array[i]);
+    }
+  }
+  return novoArray;
 }
-retornaNumerosParesElevadosADois([1,2,3,4,5,6])
-console.log(parElevado)
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
@@ -53,56 +56,153 @@ console.log(maiorNumero)
 
 
 // EXERCÍCIO 07
-function retornaObjetoEntreDoisNumeros(num1, num2) {
+function comparaDoisNumeros(num1, num2) {
+  let maiorNumero;
+  let menorNumero;
+  let maiorDivisivelPorMenor;
+
+  if (num1 > num2) {
+    maiorNumero = num1;
+    menorNumero = num2;
+  } else {
+    maiorNumero = num2;
+    menorNumero = num1;
+  }
+
+  maiorDivisivelPorMenor = maiorNumero % menorNumero === 0;
+
+  const diferenca = maiorNumero - menorNumero;
+
+  return {
+    maiorNumero: maiorNumero,
+    maiorDivisivelPorMenor: maiorDivisivelPorMenor,
+    diferenca: diferenca
+  };
 }
 
-
-retornaObjetoEntreDoisNumeros(15,30)
 
 
 // EXERCÍCIO 08
-function retornaNPrimeirosPares(n) {
-   
+function retornaNNumerosPares(n) {
+  const novoArray = [];
+  for (let number = 0; novoArray.length < n; number++) {
+    if (number % 2 === 0) {
+      novoArray.push(number);
+    }
+  }
+  return novoArray;
 }
 
-// EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {
 
+// EXERCÍCIO 09
+function checaTriangulo(a, b, c) {
+  if (a !== b && b !== c) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
 }
 
 // EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {
-  
+function segundoMaiorEMenor(array) {
+  let menor = Infinity;
+  let maior = -Infinity;
+  let segundoMenor = Infinity;
+  let segundoMaior = -Infinity;
+  let novoArray = [];
+
+  for (let i of array) {
+    if (i < menor) {
+      menor = i;
+    }
+    if (i > maior) {
+      maior = i;
+    }
+  }
+
+  for (let i of array) {
+    if (i < segundoMenor && i !== menor) {
+      segundoMenor = i;
+    }
+    if (i > segundoMaior && i !== maior) {
+      segundoMaior = i;
+    }
+  }
+  novoArray.push(segundoMaior);
+  novoArray.push(segundoMenor);
+
+  return novoArray;
 }
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
-   
-}
+function imprimeChamada(filme) {
+  let atoresConcat = "";
+  for (let i = 0; i < filme.atores.length; i++) {
+    if (i === filme.atores.length - 1) {
+      atoresConcat += filme.atores[i];
+    } else {
+      atoresConcat += filme.atores[i] + ", ";
+    }
+  }
 
+  return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${atoresConcat}.`;
+}
 // EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {
-   
+function anonimizaPessoa(pessoa) {
+  return {
+    ...pessoa,
+    nome: "ANÔNIMO"
+  };
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+  let pessoasAutorizadas = [];
+  for (const pessoa of pessoas) {
+    if (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+      pessoasAutorizadas.push(pessoa);
+    }
+  }
+  return pessoasAutorizadas;
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  let pessoasNaoAutorizadas = [];
+  for (const pessoa of pessoas) {
+    if (pessoa.altura < 1.5 || pessoa.idade <= 14 || pessoa.idade >= 60) {
+      pessoasNaoAutorizadas.push(pessoa);
+    }
+  }
+  return pessoasNaoAutorizadas;
 }
 
 // EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
-
+function calculaSaldo(contas) {
+  contas.forEach((conta) => {
+    let totalDeCompras = 0;
+    conta.compras.forEach((valor) => {
+      totalDeCompras += valor;
+    });
+    conta.saldoTotal -= totalDeCompras;
+  });
+  return contas;
 }
 
 // EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+function ordenaPorNome(consultasNome) {
+  for (let i = 0; i < consultasNome.length; i++) {
+    for (let j = 0; j < consultasNome.length - i - 1; j++) {
+      if (consultasNome[j].nome > consultasNome[j + 1].nome) {
+        const temp = consultasNome[j];
+        consultasNome[j] = consultasNome[j + 1];
+        consultasNome[j + 1] = temp;
+      }
+    }
+  }
+  return consultasNome;
 }
 
 // EXERCÍCIO 15B
