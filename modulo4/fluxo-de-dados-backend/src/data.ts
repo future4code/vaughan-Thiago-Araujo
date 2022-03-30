e58f9abdf4b7b50eceb9a34adba9a6d6948af79b
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-03-30 16:34:37 -0300

    Atividades Fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,29 @@
+export const produtos = [
+  {
+    id: "thiago-vaughan",
+    estabelecimentos: [
+      {
+        id: "28e07ef5-4c6f-4478-9998-1fae4c6d4255",
+        name: "mercado",
+        produtos: [
+          {
+            id: "1",
+            name: "Suco de Laranja",
+            price: 5
+          },
+          {
+            id: "1",
+            name: "Suco de Morango",
+            price: 5
+          },
+          {
+            id: "1",
+            name: "Suco de Uva",
+            price: 5
+          }
+        ]
+      }
+    ]
+  }
+]
+
