770747272197558ccfd38a100337be18690760ab
Thiago <94812479+thiagohenrique23@users.noreply.github.com>
2022-03-30 14:05:37 -0300

    Update execicio5.ts

--- modulo4/lista-typescript/src/execicio5.ts
+++ modulo4/lista-typescript/src/execicio5.ts
@@ -1,45 +1,27 @@
-type Adm = {
-  name: string,
-  email: string
-  role: string
+enum ROLE {
+  ADMIN = "admin",
+  USER = "user"
 }
 
-let user1: Adm = {
-  name: 'Rogério',
-  email: 'roger@email.com',
-  role: 'user'
-}
+type user = {
+  name: string;
+  email: string;
+  role: ROLE;
+};
 
-let user2: Adm = {
-  name: 'Ademir',
-  email: 'ademir@email.com',
-  role: 'admin'
-}
+const usersList: user[] = [
+  { name: "Rogério", email: "roger@email.com", role: ROLE.USER },
+  { name: "Ademir", email: "ademir@email.com", role: ROLE.ADMIN },
+  { name: "Aline", email: "aline@email.com", role: ROLE.USER },
+  { name: "Jéssica", email: "jessica@email.com", role: ROLE.USER },
+  { name: "Adilson", email: "adilson@email.com", role: ROLE.USER },
+  { name: "Carina", email: "carina@email.com", role: ROLE.ADMIN }
+];
 
-let user3: Adm = {
-  name: 'Aline',
-  email: 'aline@email.com',
-  role: 'user'
-}
-
-let user4: Adm = {
-  name: 'Jéssica',
-  email: 'jessica@email.com',
-  role: 'user'
-}
-
-let user5: Adm = {
-  name: 'Adilson',
-  email: 'adilson@email.com',
-  role: 'user'
-}
-
-let user6: Adm = {
-  name: 'Carina',
-  email: 'carina@email.com',
-  role: 'admin'
-}
-
-console.log(`Nome: ${user2.name}, Email: ${user2.email}, role: ${user2.role}`)
-console.log(`Nome: ${user6.name}, Email: ${user6.email}, role: ${user6.role}`)
-
+const execicio5 = (list: user[]): string[] => {
+  const emailsList: string[] = list
+    .filter((user) => user.role === ROLE.ADMIN)
+    .map((user) => user.email);
+  return emailsList;
+};
+console.log(execicio5(usersList));
