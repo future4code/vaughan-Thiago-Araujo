3ebcce49037dd946b6466c056368203ade052c67
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-04-18 15:25:17 -0300

    Atividades classes e encapsulamento

---
+++ modulo4/classes-encapsulamento/src/data/UserAccount.ts
@@ -0,0 +1,25 @@
+type Transaction = {
+  description: string,
+  value: number,
+  date: string
+}
+
+export class UserAccount {
+  private cpf: string;
+  private name: string;
+  private age: number;
+  private balance: number = 0;
+  private transactions: Transaction[] = [];
+
+  constructor(
+    cpf: string,
+    name: string,
+    age: number,
+  ) {
+    console.log("Chamando o construtor da classe UserAccount")
+    this.cpf = cpf;
+    this.name = name;
+    this.age = age;
+  }
+
+}
